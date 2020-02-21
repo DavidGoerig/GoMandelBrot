b0c4d5e3b350ff89c15fe6714ea724e73b3f70f2
David.goerig <David.goerig@epitech.eu>
2020-02-21 01:33:03 +0100

    finished

--- Graphical_worker.go
+++ Graphical_worker.go
@@ -23,13 +23,6 @@ const (
 	green  = 235
 	blue   = 255
 )
-
-type resultsFinal struct {
-	x int
-	y int
-	c color.Color
-}
-
 type vectors struct {
 	x int
 	y int
@@ -46,20 +39,15 @@ func mandelbrotCalc(a complex128) float64 {
 	return float64(maxEsc-i) / maxEsc
 }
 
-func farmerWorkInit(posChan <-chan vectors, results chan<- resultsFinal)  {
+func farmerWorkInit(posChan <-chan vectors, b *image.NRGBA)  {
 	scale := width / (rMax - rMin)
 	defer wg.Done()
 	for pos := range posChan {
-		fmt.Println("Sombre pute")
 		tempx := pos.x
 		tempy := pos.y
 		fEsc := mandelbrotCalc(complex(float64(tempx)/scale+rMin, float64(tempy)/scale+iMin))
 		color := color.NRGBA{uint8(red * fEsc), uint8(green * fEsc), uint8(blue * fEsc), 255}
-		results <- resultsFinal {
-			x: tempx,
-			y: tempy,
-			c: color,
-		}
+		b.Set(tempx, tempy, color)
 	}
 }
 
@@ -70,19 +58,15 @@ func launchWorkers(posChan chan <- vectors) {
 	for x := 0; x < width; x++ {
 		for y := 0; y < height; y++ {
 			posChan <- vectors{x: x, y:y}
-			fmt.Println("PosChan", x, y)
 		}
 	}
 	close(posChan)
-	wg.Wait()
 }
 
 func main() {
 	scale := width / (rMax - rMin)
 	height := int(scale * (iMax - iMin))
 	bounds := image.Rect(0, 0, width, height)
-
-	results := make(chan resultsFinal)
 	posChan := make(chan vectors)
 	b := image.NewNRGBA(bounds)
 
@@ -90,13 +74,10 @@ func main() {
 	draw.Draw(b, bounds, image.NewUniform(color.Black), image.ZP, draw.Src)
 	for nb := 0; nb < workerPool; nb += 1 {
 		wg.Add(1)
-		go farmerWorkInit(posChan, results)
+		go farmerWorkInit(posChan, b)
 	}
 	launchWorkers(posChan)
-	close(results)
-	for res := range results {
-		b.Set(res.x, res.y, res.c)
-	}
+	wg.Wait()
 	f, err := os.Create("mandelbrot.png")
 	if err != nil {
 		fmt.Println(err)
